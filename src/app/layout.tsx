d6e6ab500ac71ad3a75a922477c10e216d36a66d
Wahaj-tech <khanwahaj016@gmail.com>
2026-07-31 22:17:59 +0530

    improved architecture as well as added middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,9 +37,7 @@ export default function RootLayout({
       lang="en"
       className={`${spaceGrotesk.variable} ${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <AppShell>{children}</AppShell>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
